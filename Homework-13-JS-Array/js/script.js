16f0b52cca62aab74dda22a4f8df540243f82525
moroshandmitry <moroshandmitry@gmail.com>
2020-09-24 17:52:06 +0300

    homework practics 3

--- Homework-13-JS-Array/js/script.js
+++ Homework-13-JS-Array/js/script.js
@@ -140,3 +140,174 @@ newArray.forEach(function(item, i, newArray) {
 const newArr = [1, -1, 2, -2, 3];
 const positiveArr = newArr.filter((number) => number > 0);
 console.log(positiveArr); // 1,2,3
+
+// Life in seconds
+const secondsInAMinute = 60;
+const minutesInAnHour = 60;
+const hoursInADay = 24;
+const daysInAYear = 365;
+const age = 27;
+
+const secondsInAnHour = secondsInAMinute * minutesInAnHour;
+console.log(secondsInAnHour); // 3600
+
+const secondsInADay = secondsInAnHour * hoursInADay;
+console.log(secondsInADay); // 86400
+
+const secondsInAYear = secondsInADay * daysInAYear;
+console.log(secondsInAYear); // 31536000
+
+const lifeInSec = age * secondsInAYear; // 851472000
+console.log(lifeInSec);
+
+console.log("Суперпупердлиннаястрока".length); // 23
+
+// Run
+const codeWord1 = "обернись";
+const codeWord2 = "неужели";
+const codeWord3 = "огурцы";
+const codeWord4 = "липкие";
+const codeWord5 = "?!";
+console.log(`${codeWord1[1]}${codeWord2[1]}${codeWord3[1]}${codeWord4[1]}${codeWord5[1]}`);
+
+// slice()
+const longString = "Эта длинная строка такая длинная";
+longString.slice(4, 18); // "длинная строка"
+
+// toUpperCase(), toLowerCase()
+const myStr = "эЙ, Как дела?";
+console.log(myStr.toUpperCase());
+console.log(myStr[0].toUpperCase() + myStr.toLowerCase().slice(1));
+
+// compares with||out type
+const a = "10"
+const b = 10;
+if (a === b) {
+  console.log('compares with type');
+} else if (a == b) {
+  console.log('compares without type');
+}
+console.log(a === b); // false
+console.log(a == b); // true
+
+// compare of age to go cinema
+const newAge = 11;
+const accompanied = true;
+if (newAge > 12) {
+    console.log('Go!');
+} else if (newAge < 12 || accompanied) {
+    console.log('Go with parents!');
+} else {
+    console.log('Sorry no!');
+}
+
+// show that it's empty
+const myNullVariable = null;
+myNullVariable;
+
+// I can change values in const, but I can't overwrite dinosaurs because is a - const = ['new', 'array'];
+const dinosaurs = ["Тираннозавр",  "Велоцираптор",  "Стегозавр",  "Трицератопс",  "Брахиозавр",  "Птеранодон",  "Апатозавр",  "Диплодок"];
+dinosaurs[0] = "Тираннозавр Рекс";
+dinosaurs.pop(); // delete Диплодок in last element
+dinosaurs.push("Компсогнат"); // add Компсогнат in last element
+dinosaurs[33] = "Филосораптор"; // 8 - 32 is undefined
+console.log(dinosaurs);
+
+// Array in array
+const dinosaursAndNumbers = [3, "динозавры", ["трицератопс", "стегозавр", 3627.5], 10];
+console.log(dinosaursAndNumbers[2][1]);
+console.log(dinosaursAndNumbers.length); // length doesn't count from zero, count from 1
+console.log(dinosaursAndNumbers.length - 1); // index of array
+
+// Array push()
+var animals = [];
+animals.push("Кот");
+animals.push("Пес");
+animals.push("Лама");
+console.log(animals); // ["Кот", "Пес", "Лама"]
+console.log(animals.length - 1);
+
+// pop(), unshift()
+const animalsAnother = ["Белыймедведь", "Мартышка", "Кот", "Пес", "Лама"];
+
+const lastAnimal = animalsAnother.pop();
+console.log(lastAnimal); // "Лама"
+console.log(animalsAnother); // ["Белый медведь", "Мартышка", "Кот", "Пес"];
+
+animalsAnother.pop(); // "Пес"
+console.log(animalsAnother); // ["Белый медведь", "Мартышка", "Кот"];
+
+animalsAnother.unshift(lastAnimal);
+console.log(animalsAnother); // ["Лама", "Белый медведь", "Мартышка", "Кот"];
+
+// concat() more arrays in one
+const furryAnimals = ["Альпака", "Кольцехвостыйлемур", "Йети"];
+const scalyAnimals = ["Удав", "Годзилла"];
+const featheredAnimals = ["Ара", "Додо"];
+const allAnimals = furryAnimals.concat(scalyAnimals, featheredAnimals);
+// indexOf()
+console.log(allAnimals[6]); // Додо
+console.log(allAnimals.indexOf("Додо")); // 6
+console.log(allAnimals.indexOf("Лев")); // -1
+console.log(allAnimals); // ["Альпака", "Кольцехвостыйлемур", "Йети", "Удав", "Годзилла", "Ара", "Додо"];
+
+// join() - return as string
+const boringAnimals = ["Мартышка", "Кот", "Рыба", "Ящерица"];
+console.log(boringAnimals.join(', ')); // Мартышка, Кот, Рыба, Ящерица
+
+// Math.floor() and Math.random()
+const getRandoms = (num) => {
+  const randomIndex = Math.floor(Math.random() * num);
+};
+console.log(getRandoms(10));
+
+// random words
+const randomWords = ["Взрыв", "Пещера", "Принцесса", "Карандаш"];
+const randomIndex = Math.floor(Math.random() * 4);
+console.log(randomWords[randomIndex]);
+
+// sorry for teasing (textbook = JavaScript for Kids - Nick Morgan)
+const randomBodyParts = ["глаз", "нос", "череп"];
+const randomAdjectives = ["вонючая", "унылая", "дурацкая"];
+const randomsWords = ["муха", "выдра", "дубина", "мартышка", "крыса"];
+const randomBodyPart = randomBodyParts[Math.floor(Math.random() * randomBodyParts.length)];
+const randomAdjective = randomAdjectives[Math.floor(Math.random() * randomAdjectives.length)];
+const randomWord = randomsWords[Math.floor(Math.random() * randomWords.length)];
+const randomInsult = `У тебя ${randomBodyPart} словно ${randomAdjective} ${randomWord}!!!`;
+console.log(randomInsult);
+// result as array + join in string
+const anotherRandomInsult = ["У тебя", randomBodyPart, "словно", randomAdjective, randomWord + "!!!"].join(" ");
+console.log(anotherRandomInsult);
+
+// join() - Number more than
+const numArr = [3,2,1];
+console.log(numArr.join(" больше, чем "));
+
+// SIMPLE OBJECTS
+// The cat
+const cat = {
+  legs: 4,
+  name: "Гармония",
+  color: "Черепаховый",
+};
+
+console.log(cat.name); // Гармония
+console.log(cat["name"]); // Гармония
+
+// The dog
+const dog = {};
+dog["legs"] = 4;
+dog["age"] = 1;
+dog["name"] = "Оладушек";
+dog["color"] = "белый";
+console.log(dog); // { legs: 4, age: 1, name: "Оладушек", color: "белый" }
+
+// Array objects
+var anotherDinosaurs = [  
+  { name: "Тираннозавр рекс", period: "Верхнемеловой" },
+  { name: "Стегозавр", period: "Верхнеюрский" },
+  { name: "Платеозавр", period: "Триасовый" }
+];
+console.log(anotherDinosaurs[0]); // { name: "Тираннозавррекс", period: "Верхнемеловой" }
+console.log(anotherDinosaurs[0]["name"]); // Тираннозавр рекс
+console.log(anotherDinosaurs[1].period); // Верхнеюрский
