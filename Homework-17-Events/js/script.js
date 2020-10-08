61b7b902630aaff1218a6a262f47d8b4cb283934
moroshandmitry <moroshandmitry@gmail.com>
2020-10-08 17:52:02 +0300

    add js events workspace

---
+++ Homework-17-Events/js/script.js
@@ -0,0 +1,12 @@
+// Time
+setInterval(function () {
+  const date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  hours = hours < 10 ? `0${hours}` : `${hours}`;
+  minutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+  seconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+
+  document.querySelector(".time").innerHTML = `${hours}:${minutes}:${seconds}`;
+}, 1000);
