d04ba92257d2536d852b2c5262f726cf67b711ec
moroshandmitry <moroshandmitry@gmail.com>
2020-10-09 17:10:50 +0300

    Hw events football

--- Homework-17-Events/js/script.js
+++ Homework-17-Events/js/script.js
@@ -12,10 +12,45 @@ setInterval(function () {
 }, 1000);
 
 const btn = document.getElementById('btn')
-const text = document.getElementById('text')
+const textInput = document.getElementById('text-input')
 
 btn.addEventListener('click', getTextInConsole)
+textInput.addEventListener('focus', getTextInput)
 
 function getTextInConsole() {
-  return console.log(text.value)
+  return console.log(textInput.value)
+}
+
+function getTextInput() {
+  return textInput.style.background = 'PaleTurquoise';
+}
+
+let field = document.getElementById('field');
+let ball = document.getElementById('ball');
+
+field.addEventListener('click', football);
+
+function football (event) {
+  // получить стили getComputedStyle => ball from #ball но их нет в css, тогда возъми их из svg мяча ???
+
+  const getBallStyles =  getComputedStyle(ball);
+
+  console.log(`Получи объект стилей из svg мяча getComputedStyle(ball) ${getComputedStyle(ball)}`);
+  console.log(getComputedStyle(ball));
+  
+  const ballWidthWoPx = getBallStyles.width.slice(0, -2);
+  const ballDevWidthOnTwo = ballWidthWoPx / 2;
+
+  console.log(`Возьми ширину мяча getBallStyles.width - ${getBallStyles.width}`)
+  console.log(`Отреж px у getBallStyles.width => getBallStyles.width.slice(0, -2) и получи ${getBallStyles.width.slice(0, -2)}`)
+  console.log(`Подели ширину мяча на 2 что определить нулевую точку ${ballDevWidthOnTwo}`)
+
+  ball.style.marginLeft = (event.offsetX - ballDevWidthOnTwo) + 'px';
+  ball.style.marginTop = (event.offsetY - ballDevWidthOnTwo) + 'px';
+
+  console.log(`Двигай относительно левого края X минус пол мяча (event.offsetX - ballDevWidthOnTwo) + 'px'; получи=> ${ball.style.marginLeft}`);
+  console.log(`Двигай относительно верхнего края Y минуспол мяча (event.offsetY - ballDevWidthOnTwo) + 'px'; получи => ${ball.style.marginTop}`);
+
+  console.log(`Получи координаты поля без мяча ${event.offsetX} ${event.offsetY}`);
+
 }
